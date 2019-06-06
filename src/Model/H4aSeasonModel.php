0cb7345cf5323d57b9251ed03f617a4da5facc8f
Lünborg <Jan.Luenborg@ka.ibt.local>
2019-06-06 13:59:22 +0200

    Models for teams and season added

---
+++ src/Model/H4aSeasonModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Janborg\H4aTeams\Model;
+
+use Contao\Model;
+
+class H4aSeasonModel extends Model
+{
+  /**
+  * Table name
+  * @var string
+  **/
+  protected static $strTable = 'tl_h4aseason';
+}
---
+++ src/Model/H4aTeamsModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Janborg\H4aTeams\Model;
+
+use Contao\Model;
+
+class H4aTeamsModel extends Model
+{
+  /**
+  * Table name
+  * @var string
+  **/
+  protected static $strTable = 'tl_h4ateams';
+}
