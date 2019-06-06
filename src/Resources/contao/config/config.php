3b83f17db87f75286dae1c0bf849000b84d067cb
Lünborg <Jan.Luenborg@ka.ibt.local>
2019-06-06 14:46:32 +0200

    rename tl_h4ateams

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -5,6 +5,6 @@ array_insert($GLOBALS['BE_MOD']['content'], 1, array
 (
   'teams' => array
   (
-    'tables'      => array('tl_h4aseason', 'tl_h4ateam')
+    'tables'      => array('tl_h4aseason', 'tl_h4ateams')
     )
 ));
