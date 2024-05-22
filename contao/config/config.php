3ff495868d7b39cddb5f3a5b4fb4aa4f46b5b055
Janborg <jan-github@luenborg.eu>
2024-05-22 07:07:53 +0200

    add Models

--- contao/config/config.php
+++ contao/config/config.php
@@ -2,12 +2,21 @@
 
 declare(strict_types=1);
 
+
+use Janborg\H4aTeams\Model\H4aTeamsModel;
+use Janborg\H4aTeams\Model\H4aTeamsArchiveModel;
+
+
 /*
- * This file is part of contao-h4a_tabellen.
+ * This file is part of contao-h4a_teams.
  *
  * (c) Jan Lünborg
  *
  * @license MIT
  */
 
-$GLOBALS['BE_MOD']['content']['teams']['tables'] = ['tl_h4a_teams', 'tl_seasons'];
+$GLOBALS['BE_MOD']['content']['teams']['tables'] = ['tl_h4a_teams_archive', 'tl_h4a_teams'];
+
+// Register Models
+$GLOBALS['TL_MODELS']['tl_h4a_teams_archive'] = H4aTeamsArchiveModel::class;
+$GLOBALS['TL_MODELS']['tl_h4a_teams'] = H4aTeamsModel::class;
